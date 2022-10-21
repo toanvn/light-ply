55d5105c43e66295d8bbfd24e9e0e2051131551b
Tommy <tommy.phan@dokyo.cloud>
2022-10-21 15:12:56 +0700

    add jsonp

--- pages/embed/[fileId].js
+++ pages/embed/[fileId].js
@@ -5,6 +5,7 @@ import Router, { useRouter } from "next/router"
 import Plyr from '../../components/Plyr'
 import 'plyr-react/dist/plyr.css'
 import { useEffect, useRef } from "react"
+import fetchJsonp from "fetch-jsonp"
 
 function PlayerPage({links}) {
     const ref = useRef()
@@ -16,17 +17,32 @@ function PlayerPage({links}) {
         preload: false
     }
 
+    async function getJsonp() {
+        const apiLink = `http://localhost:8888/jsonp`
+        const res = await fetchJsonp(apiLink, {
+            jsonpCallback: 'callback'
+        })
+        const json = await res.json()
+        console.log(json)
+        return json
+    }
+
+    getJsonp()
+
     return <Container>
         <Plyr ref={ref} hlsData={cfgData} playsInline data-poster={thumbnail} />
     </Container>
 }
 
+
 PlayerPage.getInitialProps = async ({query}) => {
-    const { fileId } = query
-    const apiLink = `https://media.safechat.com/v3/media/getfile/${fileId}?link=1`
-    const res = await fetch(apiLink)
-    const json = await res.json()
-    return {links: json}
+    // const { fileId } = query
+    // const apiLink = `http://localhost:8888/jsonp`
+    // const res = await window.fetchJsonp(apiLink, {
+    //     jsonpCallbackFunction: func
+    //   })
+    // const json = await res.json()
+    return {links: {hls: ''}}
   }
 
 export default PlayerPage
